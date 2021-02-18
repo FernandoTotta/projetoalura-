0700d56f275770660139324db69acdb4a7a112a7
Fernando <f-vieira1@hotmail.com>
2021-02-18 15:45:39 -0800

    aprendendo funcoes

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -6,5 +6,17 @@ import UIKit
 let nome =  "strogonoff"
 let felicidade = 5
 let calorias = 79.5
+let vegetal: Bool = false
 
 
+//func alimentoConsumido() {
+//    print ("o alimento consumido foi: \(nome)")
+//}
+//
+//alimentoConsumido()
+
+func alimentoConsumido(nome: String, caloria: Double){
+    print ("o alimento consumido foi: \(nome) , com caloris : \(caloria)")
+}
+
+alimentoConsumido(nome:nome , caloria:calorias )
